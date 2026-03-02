51ffd7359085521b7eb13171723d836d67c56ac4
javier-anton-ordonez <javierantonordonez@gmail.com>
2026-03-02 19:20:49 +0100

    Cosas

---
+++ Programacion.sql
@@ -0,0 +1,24 @@
+-- Triggers y procesos almacenados
+
+-- Recordatorio Implementar particiones
+
+DELIMETER $$
+
+Create procedure nombre_procediminento {
+  IN p_dni varchar(12);
+  OUT p_ID bigint;
+-- parametros
+}
+BEGIN
+
+
+
+END$$
+
+DELIMETER ;
+
+
+-- Hay que poner un trigger y proceso almacenado en la practica final solo si se requiere si no nada
+
+-- Cursores
+
