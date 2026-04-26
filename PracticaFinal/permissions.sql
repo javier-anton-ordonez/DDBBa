54079e426ea4465717f078f4cd0d302bf6553f33
Melmaado <melmaado@gmail.com>
2026-04-26 18:04:33 +0200

    ix exporter user host restriction to allow Docker network connections

--- PracticaFinal/permissions.sql
+++ PracticaFinal/permissions.sql
@@ -161,9 +161,8 @@ CREATE USER IF NOT EXISTS 'dashboard_user'@'%' IDENTIFIED BY 'dashboard_password
 GRANT SELECT ON ride_hailing_db.* TO 'dashboard_user'@'%';
 
 -- Usuario para monitorización (mysqld_exporter → Prometheus → Grafana)
--- Restringido al contenedor exacto que se conecta (ride-mysqld-exporter)
-CREATE USER IF NOT EXISTS 'exporter'@'ride-mysqld-exporter' IDENTIFIED BY 'exporterpass';
-GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'ride-mysqld-exporter';
+CREATE USER IF NOT EXISTS 'exporter'@'%' IDENTIFIED BY 'exporterpass';
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
 
 FLUSH PRIVILEGES;
 
